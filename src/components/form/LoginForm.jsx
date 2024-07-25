ddf2c8567a71355dcecc6b33b45fde40a4d05923
thatikondavamshi13 <thatikondavamshi13@gmail.com>
2024-07-25 23:53:36 +0530

    firebase auth login register

--- src/components/form/LoginForm.jsx
+++ src/components/form/LoginForm.jsx
@@ -9,9 +9,59 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 const LoginForm = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const auth = getAuth();
+
+  function handleUsername(e) {
+    setEmail(e.target.value);
+  }
+
+  function handlePassword(e) {
+    setPassword(e.target.value);
+  }
+
+  const registerUser = () => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed up
+        const user = userCredential.user;
+        console.log(user);
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(error);
+        // ..
+      });
+  };
+
+  function loginUser() {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  }
+
   return (
     <Box
       component={"div"}
@@ -22,6 +72,7 @@ const LoginForm = () => {
         justifyContent: "center",
         gap: "10px",
         borderRadius: "10px",
+        // bgcolor: "red",
         flex: "2",
       }}
     >
@@ -29,14 +80,35 @@ const LoginForm = () => {
         User Login
       </Typography>
       <Paper sx={{ padding: "7px 1rem", width: "70%" }}>
-        <InputBase placeholder="username"></InputBase>
+        <InputBase placeholder="username" onChange={handleUsername}></InputBase>
       </Paper>
       <Paper sx={{ padding: "7px 1rem", width: "70%" }}>
-        <InputBase placeholder="password"></InputBase>
+        <InputBase placeholder="password" onChange={handlePassword}></InputBase>
       </Paper>
-      <Button variant="outlined" sx={{ marginTop: "10px" }}>
-        Login
-      </Button>
+      <Typography
+        component={"div"}
+        onClick={() => setIsLogin((prev) => !prev)}
+        sx={{ cursor: "pointer", marginTop: "5px" }}
+      >
+        {isLogin ? "New User?" : "Have an Account?"}
+      </Typography>
+      {isLogin ? (
+        <Button
+          onClick={loginUser}
+          variant="outlined"
+          sx={{ marginTop: "5px" }}
+        >
+          Login
+        </Button>
+      ) : (
+        <Button
+          onClick={registerUser}
+          variant="outlined"
+          sx={{ marginTop: "5px" }}
+        >
+          Regsiter
+        </Button>
+      )}
     </Box>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAUUDiYa64ExQm8PLECCU54VWL2Cj5wgB0",
+  authDomain: "todo-mui-vk.firebaseapp.com",
+  databaseURL: "https://todo-mui-vk-default-rtdb.firebaseio.com",
+  projectId: "todo-mui-vk",
+  storageBucket: "todo-mui-vk.appspot.com",
+  messagingSenderId: "829070457688",
+  appId: "1:829070457688:web:c24526d6cdbbb58e21032d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
