ec56306b439a9e6ccc5b45150b345fd749763f3a
vamshi13vk <thatikondavamshi13@gmail.com>
2024-08-17 14:42:19 +0530

    ability to copy added to contextMenu of a todo

--- src/components/ui/todo/todomenu/TodoMenu.jsx
+++ src/components/ui/todo/todomenu/TodoMenu.jsx
@@ -2,20 +2,32 @@ import { Box, Menu, MenuItem } from "@mui/material";
 import React, { useContext, useState } from "react";
 import { appDataContext } from "../../../../store/AppDataProvider";
 import useFetch from "../../../../hooks/useFetch";
+import { v4 as uuidv4 } from "uuid";
 
 const TodoMenu = ({ anchorPosition, setContextMenu, isOpen, task }) => {
   const [, updateTask, isUpdateSyncing, isUpdateSuccess] = useFetch(
     "UPDATE",
     "/tasks/"
   );
+  const [, postTask, isPostSyncing, isPostSuccess] = useFetch(
+    "POST",
+    "/tasks.json"
+  );
   const { customSidebarItems, setTasks, sidebarItems, currentSidebarItemId } =
     useContext(appDataContext);
   const [moveAnchor, setMoveAnchor] = useState(null);
   const isMoveOpen = Boolean(moveAnchor);
+  const [copyAnchor, setCopyAnchor] = useState(null);
+  const isCopyOpen = Boolean(copyAnchor);
   function handleMove(e) {
     e.preventDefault();
     e.stopPropagation();
     setMoveAnchor(e.currentTarget);
+  }
+  function handleCopy(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    setCopyAnchor(e.currentTarget);
   }
 
   function handleMoveToList(item) {
@@ -48,6 +60,31 @@ const TodoMenu = ({ anchorPosition, setContextMenu, isOpen, task }) => {
     });
     setMoveAnchor(null);
   }
+  async function handleCopyToList(item) {
+    //move to the list with id = item.id
+    // 1. get the task object
+    const curTask = task;
+    // 2. get the list type id
+    const toCopyListId = item.id;
+
+    // 3. add a new task with updated id and updated listTypeId
+    const id = uuidv4();
+    const updatedTask = { ...curTask, id: id, listTypeId: toCopyListId };
+    setTasks((prev) => {
+      return [...prev, updatedTask];
+    });
+    // 4. Post a new task with the updated id and updated ListTypeId
+    const { name: key } = await postTask(updatedTask);
+    // 5. update the key at local state when new task is added
+    setTasks((prevTasks) => {
+      return prevTasks.map((task) => {
+        if (task.id == id) {
+          return { ...task, key };
+        } else return task;
+      });
+    });
+    setMoveAnchor(null);
+  }
 
   return (
     <Box>
@@ -57,7 +94,7 @@ const TodoMenu = ({ anchorPosition, setContextMenu, isOpen, task }) => {
         onClose={() => setContextMenu(null)}
         anchorReference="anchorPosition"
       >
-        <MenuItem>Copy</MenuItem>
+        <MenuItem onClick={handleCopy}>Copy</MenuItem>
         <MenuItem onClick={handleMove}>Move</MenuItem>
         <MenuItem>Delete</MenuItem>
       </Menu>
@@ -75,6 +112,25 @@ const TodoMenu = ({ anchorPosition, setContextMenu, isOpen, task }) => {
           .map((item) => {
             return (
               <MenuItem onClick={() => handleMoveToList(item)} key={item.id}>
+                {item.name}
+              </MenuItem>
+            );
+          })}
+      </Menu>
+      <Menu
+        open={isCopyOpen}
+        anchorEl={copyAnchor}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        onClose={() => setCopyAnchor(null)}
+      >
+        {[...sidebarItems, ...customSidebarItems]
+          .filter((item) => item.id !== currentSidebarItemId)
+          .map((item) => {
+            return (
+              <MenuItem onClick={() => handleCopyToList(item)} key={item.id}>
                 {item.name}
               </MenuItem>
             );
